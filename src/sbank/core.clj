38e6cb9dcdd0df2edfb378c10829a1c49e63dcd6
Rafael Rondao <rafael.rondao@gmail.com>
2022-03-16 11:21:54 -0300

    Add withdraw service
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/sbank/core.clj
+++ src/sbank/core.clj
@@ -1,6 +1,6 @@
 (ns sbank.core
   (:import [com.twitter.finagle Http Service]
-           [com.twitter.util Await Future]
+           [com.twitter.util Future]
            [com.twitter.finagle.http Response])
   (:require [clojure.data.json :as json]))
 
@@ -27,8 +27,28 @@
          (str (get @bank (:id deposit-order))))
         (Future/value response)))))
 
+(defn make-withdraw-service
+  [data-parser]
+  (proxy [Service] []
+    (apply [request]
+      (let [response (Response/apply)
+            withdraw-order (data-parser (.getContentString request))]
+        (swap! bank
+               update
+               (:id withdraw-order)
+               #(- (or % 0) (:value withdraw-order)))
+        (.setContentString
+         response
+         (str (get @bank (:id withdraw-order))))
+        (Future/value response)))))
+
 (defn serve-deposit-service
   []
-  (Http/serve ":8080" (make-deposit-service (make-json-parser))))
+  (Http/serve ":20000" (make-deposit-service (make-json-parser))))
 
-(Await/serve (serve-deposit-service))
+(defn serve-withdraw-service
+  []
+  (Http/serve ":20001" (make-withdraw-service (make-json-parser))))
+
+(serve-deposit-service)
+(serve-withdraw-service)
